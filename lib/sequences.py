9d57361e9fc9ce4acebd5648b9b8f3b84854a9ef
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-09-04 23:56:32 +0300

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  
+        return
+    
+    fib_sequence = [0, 1]
+    
+    while len(fib_sequence) < length:
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+    
+    print(fib_sequence[:length])
